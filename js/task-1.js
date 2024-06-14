57d209902a6964a38d7bde7b4427df9cfdbd5520
Nikitin <161200278+Nikitincv@users.noreply.github.com>
2024-06-14 22:24:56 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
 
-// const cat = document.querySelectorAll(".item");
-// console.log("Number of categories: "  + cat.length );
+const cat = document.querySelectorAll(".item");
+console.log("Number of categories: "  + cat.length );
 
-// cat.forEach((category) => {
-//     const categoryTitle = category.querySelector('h2').textContent;
-//     const categoryItem = category.querySelectorAll("li").length;
-//     console.log(`Category ${categoryTitle}`);
-//     console.log(`Elements: ${categoryItem}`);
-// });
+cat.forEach((category) => {
+    const categoryTitle = category.querySelector('h2').textContent;
+    const categoryItem = category.querySelectorAll("li").length;
+    console.log(`Category ${categoryTitle}`);
+    console.log(`Elements: ${categoryItem}`);
+});
